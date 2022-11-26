9ee6edcd160232c5fa4abad2fe5b198b0a984606
ketan1829 <ketan.salvi21@gmail.com>
2022-11-26 22:53:12 +0530

    brand

--- app/javascript/shared/mixins/globalConfigMixin.js
+++ app/javascript/shared/mixins/globalConfigMixin.js
@@ -1,8 +1,8 @@
 export default {
   methods: {
     useInstallationName(str = '', installationName) {
-      // return str.replace(/Aligned/g, installationName);
-      return "Aligned";
+      return str.replace(/Aligned/g, installationName);
+      // return "Aligned";
     },
   },
 };
