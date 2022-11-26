2a4b89b9375b0b415677336b1f3c26af445d89a1
ketan1829 <ketan.salvi21@gmail.com>
2022-11-26 15:10:36 +0530

    installationName update

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -43,7 +43,7 @@ const state = {
 export const getters = {
   get: $state => $state,
   isOnChatwootCloud: $state => $state.deploymentEnv === 'cloud',
-  isACustomBrandedInstance: $state => $state.installationName !== 'Chatwoot',
+  isACustomBrandedInstance: $state => $state.installationName !== 'Aligned CSP',
 };
 
 export const actions = {};
