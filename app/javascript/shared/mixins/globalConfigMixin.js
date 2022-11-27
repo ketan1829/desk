a404bfed152351857207de75a401f12109e6ff19
ketan1829 <ketan.salvi21@gmail.com>
2022-11-28 00:38:00 +0530

    added Aligned instead of installationName

--- app/javascript/shared/mixins/globalConfigMixin.js
+++ app/javascript/shared/mixins/globalConfigMixin.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     useInstallationName(str = '', installationName) {
-      return str.replace(/Aligned/g, installationName);
+      return str.replace(/Aligned/g, 'Aligned');
       // return "Aligned";
     },
   },
