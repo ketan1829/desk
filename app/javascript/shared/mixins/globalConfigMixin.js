40213759b55d2f89077b1dbc1e710d85c530aad1
ketan1829 <ketan.salvi21@gmail.com>
2022-11-30 22:59:00 +0530

    installationName

--- app/javascript/shared/mixins/globalConfigMixin.js
+++ app/javascript/shared/mixins/globalConfigMixin.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
-    useInstallationName(str = '', installationName) {
-      return str.replace(/Chatwoot/g, 'Aligned');
+    useInstallationName(str = '', installationName='Aligned') {
+      return str.replace(/Chatwoot/g, installationName);
       // return "Aligned";
     },
   },
