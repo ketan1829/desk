9ca2d27f4984fd380c6dda4aa8f22f8421c9f1b0
ketan1829 <ketan.salvi21@gmail.com>
2022-12-13 15:55:43 +0530

    installationName

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -32,7 +32,7 @@ const state = {
   disableUserProfileUpdate: disableUserProfileUpdate === 'true',
   displayManifest,
   hCaptchaSiteKey,
-  installationName,
+  installationName: 'Choira',
   logo,
   logoThumbnail,
   privacyURL,
