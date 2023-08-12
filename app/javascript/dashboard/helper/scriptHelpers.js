93822607abe2158608d2fa12f90d45d3281cc1d1
NitinNR <46310938+NitinNR@users.noreply.github.com>
2023-08-12 19:07:22 +0000

    rename to window.$lifeel

--- app/javascript/dashboard/helper/scriptHelpers.js
+++ app/javascript/dashboard/helper/scriptHelpers.js
@@ -42,19 +42,19 @@ const initializeAudioAlerts = user => {
 
 export const initializeChatwootEvents = () => {
   window.bus.$on(CHATWOOT_RESET, () => {
-    if (window.$chatwoot) {
-      window.$chatwoot.reset();
+    if (window.$lifeel) {
+      window.$lifeel.reset();
     }
   });
   window.bus.$on(CHATWOOT_SET_USER, ({ user }) => {
-    if (window.$chatwoot) {
-      window.$chatwoot.setUser(user.email, {
+    if (window.$lifeel) {
+      window.$lifeel.setUser(user.email, {
         avatar_url: user.avatar_url,
         email: user.email,
         identifier_hash: user.hmac_identifier,
         name: user.name,
       });
-      window.$chatwoot.setCustomAttributes({
+      window.$lifeel.setCustomAttributes({
         signedUpAt: user.created_at,
         cloudCustomer: 'true',
         account_id: user.account_id,
