abf6f7fa446d0ac82b2bcd5b992b4225a4418822
NitinNR <46310938+NitinNR@users.noreply.github.com>
2023-08-30 10:04:42 +0000

    aspen new end conv logo changed

--- app/javascript/widget/components/HeaderActions.vue
+++ app/javascript/widget/components/HeaderActions.vue
@@ -6,11 +6,19 @@
       :title="$t('END_CONVERSATION')"
       @click="resolveConversation"
     >
-      <fluent-icon
+      <!-- <fluent-icon
         icon="sign-out"
         size="22"
         :class="$dm('text-black-900', 'dark:text-slate-50')"
       />
+      -->
+
+      <img
+        src="/assets/aspen/end_conv.png"
+        alt="Sign Out"
+        class="aspen-icon-image"
+      />
+
     </button>
     <button
       v-if="showPopoutButton"
@@ -130,6 +138,11 @@ export default {
   }
   .rn-close-button {
     display: block !important;
+  }
+
+  .aspen-icon-image {
+    width: 22px;
+    height: auto;
   }
 }
 </style>
